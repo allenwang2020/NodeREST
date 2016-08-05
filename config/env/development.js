6b8f7a2b22d04f43dbe89e6d735a1ddd1d3095cf
allenwang2020 <allenwang2020@gmail.com>
2016-08-05 16:04:30 +0800

    修改開發環境變數的server IP位值
    修改開發環境變數的server IP位值

--- config/env/development.js
+++ config/env/development.js
@@ -1,6 +1,6 @@
 module.exports  = {
     sessionSecret : 'developmentSessionSecret',
     db: 'mongodb://localhost/products-demo',
-    server_ip:'127.0.0.1',
+    server_ip:'52.36.188.46',
     server_port:'80'	
 }
