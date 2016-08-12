c2fa83910fd54325ed32cb6078ec68db2db0f443
allenwang2020 <allenwang2020@gmail.com>
2016-08-12 13:30:54 +0800

    all

--- config/env/env.js
+++ config/env/env.js
@@ -1,6 +1,6 @@
 module.exports  = {
     sessionSecret : 'developmentSessionSecret',
-    db: 'mongodb://mongo:27018/products-demo',
+    db: 'mongodb://db:27017/products-demo',
     server_ip:'127.0.0.1',
     server_port:'8080'	
 }
