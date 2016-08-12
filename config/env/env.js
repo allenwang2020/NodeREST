e31b3ce0605527f29a66c4a03faa0a903663b42d
allenwang2020 <allenwang2020@gmail.com>
2016-08-12 10:20:48 +0800

    modify bug

--- config/env/env.js
+++ config/env/env.js
@@ -1,6 +1,6 @@
 module.exports  = {
     sessionSecret : 'developmentSessionSecret',
-    db: 'mongodb://mongo:27017/products-demo',
+    db: 'mongodb://mongo:27018/products-demo',
     server_ip:'127.0.0.1',
     server_port:'8080'	
 }
