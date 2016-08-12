7042662c03f4766a6f57667bf74fe298d2ab9e4b
allenwang2020 <allenwang2020@gmail.com>
2016-08-12 13:57:36 +0800

    all

--- config/env/env.js
+++ config/env/env.js
@@ -1,6 +1,5 @@
 module.exports  = {
     sessionSecret : 'developmentSessionSecret',
-    db: 'mongodb://db:27017/products-demo',
     server_ip:'127.0.0.1',
     server_port:'8080'	
 }
--- config/mongoose.js
+++ config/mongoose.js
@@ -1,13 +1,12 @@
-var config = require('./config'),
-	mongoose = require('mongoose');
+var mongoose = require('mongoose');
 
 
 module.exports = function(){
-    var db = mongoose.connect(config.db,function(err) {
+    var db = mongoose.connect('mongodb://db:27017/products-demo',function(err) {
         if(err) {
             console.log('connection error', err);
         } else {
-            console.log('connection successful : ' + config.db);
+            console.log('connection successful : ' + 'mongodb://db:27017/products-demo');
         }
     });
     return db;
