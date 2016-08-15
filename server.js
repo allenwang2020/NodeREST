bc7dc80255943a06df6fd0b7afcc5f6bb2d3cd02
allenwang2020 <allenwang2020@gmail.com>
2016-08-15 22:04:49 +0800

    test

--- server.js
+++ server.js
@@ -1,60 +1,12 @@
-
-// Dependencies
+// Load the http module to create an http server.
 var http = require('http');
-var express = require('express');
-var bodyParser = require('body-parser');
-//var morgan = require('morgan');
-var config = require('./config/config');
 
-
-// MongoDB
-//var Mongoose = require('./config/mongoose');
-//var db =  new Mongoose();
-
-// Express
-var app = express();
-
-app.use(express.static(__dirname + '/public'));
-
-app.set('superSecret', config.sessionSecret); // secret variable
-
-//use body parser so we can get info from POST and/or URL parameters
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-//use morgan to log requests to the console
-//app.use(morgan('dev'));
-
-//var logger = require('morgan');
-//var fs = require('fs')
-//var FileStreamRotator = require('file-stream-rotator')
-//var logDirectory = __dirname + '/logs'
-
-// ensure log directory exists
-//fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory)
-
-// create a rotating write stream
-//var accessLogStream = FileStreamRotator.getStream({
-//  filename: logDirectory + '/access-%DATE%.log',
-//  frequency: 'daily',
-//  verbose: false
-//})
-//// setup the logger
-//app.use(logger('combined', {stream: accessLogStream}))
-
-
-// Routes
-app.use('/api', require('./routes/api'));
-
-// Start server
-
-var port = config.server_port || 8080
-, ip = config.server_ip || "127.0.0.1";
-//app.listen(port, ip, function() {
-//  console.log('Express server listening on %d', port);
-//});
-
-var server = http.createServer(app);
-server.listen(port, function() {
-  console.log('Server running on ' + ip+":"+port);
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write("<h1>Hello World from Node.js app!!!</h1>\n");
+  response.end("<script type=\"text/javascript\">document.write('<h3><a href=\"http://' + location.host + ':8080/\">' + 'Tomcat Application on port 8080</a></h3>');</script>");
 });
+
+// Listen on port 8000, IP defaults to 127.0.0.1
+server.listen(8000);
