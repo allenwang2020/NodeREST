e163add4b07a7be0fc3d1517a809db3e6545bea3
allenwang2020 <allenwang2020@gmail.com>
2019-10-08 10:31:17 +0800

    modify to AWS

--- web/conf/env/env.js
+++ web/conf/env/env.js
@@ -1,6 +1,6 @@
 module.exports  = {
     sessionSecret : 'developmentSessionSecret',
-    server_ip:'192.168.99.103',
+    server_ip:'100.21.37.216',
     server_port:'3000',
     redis_ip:'localhost',
     redis_port:'3678'
