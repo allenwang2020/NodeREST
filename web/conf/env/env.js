3f635fb59468c52696a75ff50134dc8504f8b80c
allenwang2020 <allenwang2020@gmail.com>
2019-10-08 13:43:45 +0800

    test

--- web/conf/env/env.js
+++ web/conf/env/env.js
@@ -1,7 +1,7 @@
 module.exports  = {
     sessionSecret : 'developmentSessionSecret',
-    server_ip:'127.0.0.1',
-    server_port:'3000',
+    server_ip:'100.21.37.216',
+    server_port:'80',
     redis_ip:'localhost',
     redis_port:'3678'
 }
