2c7aef28fecd3fff776a5f51931a8a8df94389b7
allenwang2020 <allenwang2020@gmail.com>
2019-11-01 19:24:54 +0800

    修改mongoDB 路徑

--- web/conf/env/env.js
+++ web/conf/env/env.js
@@ -1,7 +1,7 @@
 module.exports  = {
     sessionSecret : 'developmentSessionSecret',
-    server_ip:'localhost',
+    server_ip:'127.0.0.1',
     server_port:'3000',
-    redis_ip:'localhost',
+    redis_ip:'127.0.0.1',
     redis_port:'3678'
 }
